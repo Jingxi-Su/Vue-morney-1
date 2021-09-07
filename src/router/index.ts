7060d3f32f990fccef3470d693aef9272bbb9a0d
jingxi <774874572@qq.com>
2021-09-07 22:54:01 +0800

    添加404页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,30 +3,35 @@ import VueRouter from 'vue-router';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',//默认路径
-    redirect: '/money'
-  },
-  {
-    path: '/money',
-    component: Money
-  },
-  {
-    path: '/labels',
-    component: Labels
-  },
-  {
-    path: '/statistics',
-    component: Statistics
-  }
+    {
+        path: '/',//默认路径
+        redirect: '/money'
+    },
+    {
+        path: '/money',
+        component: Money
+    },
+    {
+        path: '/labels',
+        component: Labels
+    },
+    {
+        path: '/statistics',
+        component: Statistics
+    },
+    {
+        path: '*',//*表示匹配其他所有路径
+        component: NotFound
+    }
 ];
 
 const router = new VueRouter({
-  routes
+    routes
 });
 
 export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>当前页面不存在，请检查网址是否存在</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+<!--      第二种写法<a href="#/">返回首页</a>-->
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NotFound'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
